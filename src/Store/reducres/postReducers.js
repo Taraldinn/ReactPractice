26ef9c7cb8dfd274daf78695a2d74167834b6ca4
Taraldinn <kferdoush617@gmail.com>
2024-04-17 00:27:30 +0600

    post reducer

--- src/Store/reducres/postReducers.js
+++ src/Store/reducres/postReducers.js
@@ -16,17 +16,42 @@ function postReducer(state = initState, action){
 
 // reducer is just a simple function that takes two arguments, state and action, and returns the next state.
 // It's called a reducer because it's the type of function you would pass to Array.prototype.reduce(reducer, ?initialValue) to reduce an array to a single value.
-        if (action.type === 'CREATE_POST'){
-            return {
-                ...state,
-                post : state.post.concat(action.payload),
-            } 
+        // if (action.type === 'CREATE_POST'){
+        //     return {
+        //         ...state,
+        //         post : state.post.concat(action.payload),
+        //     } 
 
-        }
-        else if (action.type === 'UPDATE_POST'){}
-        else if (action.type === 'DELETE_POST'){}
-        else {
-            return state;
+        // }
+        // else if (action.type === 'UPDATE_POST'){
+
+        // }
+        // else if (action.type === 'DELETE_POST'){}
+        // else {
+        //     return state;
+        // }
+
+        switch (action.type) {
+            case 'CREATE_POST':
+                return{
+                    ...state,
+                    post: state.post.concat(action.payload),
+                    
+                }
+            case 'UPDATE_POST':
+                return{
+                    ...state,
+
+                }
+            case 'DELETE_POST':
+                return{
+                    ...state,
+                    
+                }
+
+            default:
+                return state;
+                
         }
 
 }
--- src/Store/reducres/rootReducers.js
+++ src/Store/reducres/rootReducers.js
@@ -1,9 +1,10 @@
 import {combineReducers} from "@reduxjs/toolkit";
-
+import postReducer from "./postReducer";
 
 const rootReducer = combineReducers({
-    
+    posts: postReducer,
 })
 
 
 export default rootReducer;
+
